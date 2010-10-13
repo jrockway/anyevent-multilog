f32aac4097ae0ef51a719cb4dd17dded7b07c0ae
Jonathan Rockway <jon@jrock.us>
2010-10-13 13:18:39 -0500

    add docs

--- lib/AnyEvent/Multilog.pm
+++ lib/AnyEvent/Multilog.pm
@@ -1,4 +1,5 @@
 package AnyEvent::Multilog;
+# ABSTRACT: event-driven interface to a multilog process
 use Moose;
 use MooseX::Types::Path::Class qw(File);
 
@@ -206,3 +207,126 @@ sub shutdown {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    my $log = AnyEvent::Multilog->new(
+        script => [qw{t +* ./log}],
+    );
+
+    $log->start;
+    $log->push_write('log message');
+    $log->push_write('another log message');
+    $log->rotate;
+    $log->push_write('another log message in a new log file');
+    $log->shutdown;
+
+=head1 DESCRIPTION
+
+This module makes it easy to log via a multilog process.  It handles
+spawning the multilog process and handling its errors.
+
+=head1 ATTRIBUTES
+
+=head2 script
+
+Required.
+
+This is an ArrayRef representing the multilog script that describes
+how to log.  See the L<multilog|multilog man page> for more
+information on what this script is and how to write one.
+
+Note that the shell is never invoked, so you don't need to escape
+anything from the shell.
+
+To select all lines, add a tai64n timestamp, and log to a directory
+called "log", your script should be C<['t', '+*', './log']>.
+
+=head2 multilog
+
+Optional.
+
+The path to the multilog binary.  By default, checks C<$PATH> and uses
+the one in there.
+
+=head2 on_exit
+
+Optional.
+
+Coderef that is called when the multilog process exists, successfully
+or otherwise.
+
+Your coderef is passed three arguments, a boolean indicating
+successful exit, a message indicating why multilog exited, and the
+L<AnyEvent::Subprocess::Done> object representing the exited
+subprocess.
+
+=head2 on_error
+
+Optional.
+
+Coderef to be called when multilog writes something to stderr.  It's
+assumed that logging can't proceed after something is read from
+stderr, so all methods will die regardless of whether or not you
+handle this callback.  Handling this event lets you proactively spawn
+a new logger and kill this one without losing any messages.
+
+Patch welcome to make this automatic.  I can't get multilog to die on
+my machine.
+
+=head1 METHODS
+
+=head2 start
+
+Call this when you are ready to fork off the multilog and start
+logging.  If you call another method before calling this, the results
+are undefined.
+
+=head2 push_write
+
+Send a line to multilog.  If you don't provide a newline, one will be
+provided for you.
+
+=head2 rotate
+
+Ask multilog to rotate the log right now.
+
+=head2 push_shutdown
+
+Ask multilog to shut down after writing the current line to disk.  Any
+pending data is made available via C<< $self->leftover_data >>, which
+can be checked for with C<< $self->has_leftover_data >>.
+
+When multilog is done writing, your C<on_exit> callback will be
+called.
+
+=head2 shutdown
+
+Shutdown immediately by closing the file descriptor that multilog is
+reading from.
+
+When multilog is done writing, your C<on_exit> callback will be
+called.
+
+=head2 is_shutdown
+
+Returns true if multilog is done.
+
+=head2 has_leftover_data
+
+Returns true if multilog exited without reading some data we asked it
+to log.
+
+=head2 leftover_data
+
+Returns any data that multilog did not write before exiting.
+
+=head2 has_errors
+
+Returns true if errors were encourntered.
+
+=head2 errors
+
+Returns a list of errors that were encountered.
